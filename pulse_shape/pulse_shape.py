5f8c37df167bc3ed9e639f090229e54862566f3c
matantorem <matantorem@mail.tau.ac.il>
2026-03-22 11:32:52 +0200

    pulse shape code

---
+++ pulse_shape/pulse_shape.py
@@ -0,0 +1,36 @@
+from typing import Literal
+
+import numpy as np
+import pydantic
+
+class PulseShapeConfig(pydantic.BaseModel):
+    pulse_shape_type: Literal["rect"]
+    normalization_type: Literal["None", "norm_2", "norm_1"]
+
+class PulseShape:
+    def __init__(self, config: PulseShapeConfig):
+        self.config = config
+
+    def generate_pulse_shape(self, sample_rate: float, symbol_time: float):
+        if self.config.pulse_shape_type == "rect":
+            rect_pulse = self.generate_rect_pulse(sample_rate, symbol_time)
+            return rect_pulse
+        else:
+            raise ValueError(f"Unknown pulse shape type: {self.config.pulse_shape_type}")
+
+    def generate_rect_pulse(self, sample_rate: float, symbol_time: float) -> np.ndarray:
+        rect_pulse = np.ones(int(sample_rate * symbol_time))
+        normalized_pulse = self.normalize_pulse(rect_pulse)
+        return normalized_pulse
+
+    def normalize_pulse(self, pulse: np.ndarray) -> np.ndarray:
+        if self.config.normalization_type == "None":
+            return pulse
+        elif self.config.normalization_type == "norm_2":
+            normalized_pulse = pulse / np.linalg.norm(pulse, ord=2)
+            return normalized_pulse
+        elif self.config.normalization_type == "norm_1":
+            normalized_pulse = pulse / np.linalg.norm(pulse, ord=1)
+            return normalized_pulse
+        else:
+            raise ValueError(f"Unknown normalization type: {self.config.normalization_type}")
